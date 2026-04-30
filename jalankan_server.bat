71250d5a6ee7ed16cc1fcad9cc32307bcbe2a60f
madenp <50958903+madenp@users.noreply.github.com>
2026-04-30 20:48:27 +0800

    Add files via upload

---
+++ jalankan_server.bat
@@ -0,0 +1,9 @@
+@echo off
+title Server Ruang Baca
+echo ===================================================
+echo Menjalankan Server Ruang Baca...
+echo Pastikan Anda belum menutup jendela ini selama aplikasi digunakan!
+echo ===================================================
+echo.
+node server.js
+pause
